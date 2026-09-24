6ab95d857f98f6165c92db3e7ca0ae38c897bfd2
Soumik <soumyadey983@gmail.com>
2026-09-24 22:56:01 +0530

    config.js

--- config.js
+++ config.js
@@ -1,10 +1,3 @@
-/**
- * ====================================================================
- *                 ⚡ ARCEUS XD - CONFIGURATION ⚡
- *                       Owner: SOUMIK
- * ====================================================================
- */
-
 const fs = require('fs');
 if (fs.existsSync('.env')) require('dotenv').config({ path: '.env' });
 
@@ -12,12 +5,12 @@ module.exports = {
     // BOT METADATA
     BOT_NAME: process.env.BOT_NAME || '𝗔𝗥𝗖𝗘𝗨𝗦 𝗫𝗗',
     BOT_VERSION: process.env.BOT_VERSION || 'v12.0.0',
-    PLATFORM: process.env.PLATFORM || '🚉 Railway',
+    PLATFORM: process.env.PLATFORM || '',
     
     // OWNER DETAILS
     OWNER_NAME: process.env.OWNER_NAME || '𝗦𝗢𝗨𝗠𝗜𝗞',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '918420027377',
-    SUDO: process.env.SUDO ? process.env.SUDO.split(',') : ['918420027377'],
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '917602624961',
+    SUDO: process.env.SUDO ? process.env.SUDO.split(',') : [''],
 
     // BOT OPERATION
     PREFIX: process.env.PREFIX || '.',
@@ -35,16 +28,9 @@ module.exports = {
     ANTI_BADWORD: process.env.ANTI_BADWORD === 'true',
     WELCOME: process.env.WELCOME !== 'false',
     GOODBYE: process.env.GOODBYE !== 'false',
-
-    // TELEGRAM PAIRING & NOTIFICATIONS
-    TG_BOT_TOKEN: process.env.TG_BOT_TOKEN || '8884910225:AAHUXPA5I1m2OefTIt2fzMangpAdS_gkQBw', // Token from @BotFather
-    TG_OWNER_ID: process.env.TG_OWNER_ID || '8697263029',   // Telegram ID from @userinfobot
-    USE_PAIRING_CODE: process.env.USE_PAIRING_CODE !== 'false',
-    PAIRING_NUMBER: process.env.PAIRING_NUMBER || process.env.OWNER_NUMBER || '918420027377',
     
     // AI ENGINE
     GEMINI_API_KEY: process.env.GEMINI_API_KEY || '',
-    
     // TOTAL COMMANDS
     COMMAND_COUNT: 480
 };
